0ac89b4aca5f9d87ad6500c614af140c0b875645
Shivam Vijaywargi <vijaywargishivam@gmail.com>
2024-10-17 20:14:17 +0530

    feat: save logs in dedicated files✨

--- src/common/middlewares/pino-logger.middleware.ts
+++ src/common/middlewares/pino-logger.middleware.ts
@@ -1,16 +1,33 @@
 import { logger } from "hono-pino";
 import pino from "pino";
-import pretty from "pino-pretty";
 
 import env from "@/env";
+
+const transport = pino.transport({
+  targets: [
+    {
+      target: "pino/file",
+      options: { destination: "logs/combined.log", mkdir: true },
+    },
+    {
+      level: "error",
+      target: "pino/file",
+      options: { destination: "logs/error.log", mkdir: true },
+    },
+    {
+      target: env.NODE_ENV === "production" ? "pino/file" : "pino-pretty",
+    },
+  ],
+});
 
 export function pinoLogger() {
   return logger({
     pino: pino(
       {
         level: env.LOG_LEVEL || "info",
+        timestamp: pino.stdTimeFunctions.isoTime,
       },
-      env.NODE_ENV === "production" ? undefined : pretty(),
+      transport,
     ),
     http: {
       reqId: () => crypto.randomUUID(),
