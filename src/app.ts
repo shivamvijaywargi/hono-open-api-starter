c9bd4ba8ad1c2a3cec6d8eb3dfa45bef39a5e64e
Shivam Vijaywargi <vijaywargishivam@gmail.com>
2024-10-17 15:52:39 +0530

    feat: setup pino logger✨

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,24 @@
+import type { PinoLogger } from "hono-pino";
+
 import { OpenAPIHono } from "@hono/zod-openapi";
-import { logger } from "hono/logger";
 
 import { notFoundMiddleware } from "./common/middlewares/not-found.middleware";
 import { onErrorMiddleware } from "./common/middlewares/on-error.middleware";
+import { pinoLogger } from "./common/middlewares/pino-logger.middleware";
 
-export const app = new OpenAPIHono();
+interface AppBindings {
+  Variables: {
+    logger: PinoLogger;
+  };
+}
 
-app.use(logger());
+export const app = new OpenAPIHono<AppBindings>();
+
+app.use(pinoLogger());
 
 app.get("/", (c) => {
+  c.var.logger.info("WOOOOOOW");
+  c.var.logger.debug("WOOOOOOW");
   return c.text("Hello Hono!");
 });
 
